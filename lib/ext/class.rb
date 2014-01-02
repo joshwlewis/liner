665f3a41c96527bcd61fda1bad1de0412ef2f358
Josh Lewis <josh.w.lewis@gmail.com>
2014-01-01 20:55:14 -0600

    Prevent a few methods from being included/extended.

--- lib/ext/class.rb
+++ lib/ext/class.rb
@@ -1,11 +1,5 @@
 Class.class_eval do
   def liner(*keys)
-    keys = keys.map(&:to_sym)
-    define_method(:liner_keys){ keys }
-    include Liner
-    keys.each do |key|
-      define_method(key){ self[key] }
-      define_method("#{key}="){ |value| self[key] = value }
-    end
+    Liner.apply self, *keys
   end
 end
--- lib/liner.rb
+++ lib/liner.rb
@@ -7,21 +7,26 @@ require 'liner/serializable'
 require 'ext/class'
 
 module Liner
-  def self.new(*keys)
-    keys = keys.map(&:to_sym)
-    Class.new do |klass|
-      define_method(:liner_keys){ keys }
-      klass.send :include, Liner
-      keys.each do |key|
-        define_method(key){ self[key] }
-        define_method("#{key}="){ |value| self[key] = value }
-      end
-    end
-  end
-
   def self.included(base)
     [Base, Hashable, Equalizable, Inspectable, Serializable].each do |mod|
       base.send :include, mod
     end
   end
+end
+
+def Liner.new(*keys)
+  apply Class.new, *keys
+end
+
+def Liner.apply(base, *keys)
+  keys = keys.map(&:to_sym).uniq
+  base.class_eval do
+    define_method(:liner_keys){ keys }
+    include Liner
+    keys.each do |key|
+      define_method(key){ self[key] }
+      define_method("#{key}="){ |value| self[key] = value }
+    end
+  end
+  base
 end
