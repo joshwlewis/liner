8458d16a52d2016a80f17b87e58921ff9087557a
Josh Lewis <josh.w.lewis@gmail.com>
2014-03-02 16:12:43 -0600

    Move attribute string into it's own method.

--- lib/liner/inspectable.rb
+++ lib/liner/inspectable.rb
@@ -4,9 +4,16 @@ module Liner
     # @return [String]
     # @api public
     def inspect
-      attribute_string = liner.map{|k,v| "#{k}=#{v.inspect}"}.join(', ')
       "#<#{self.class} #{attribute_string}>"
     end
     alias :to_s :inspect
+
+    # List all the liner attributes as a string, used for inspection.
+    # @return [String]
+    # @api private
+    def attribute_string
+      liner.map { |k,v| "#{k}=#{v.inspect}" }.join(', ')
+    end
+    private :attribute_string
   end
 end
