f06f93db273cf660d1bbbf92d5e826a42c924ff1
Josh Lewis <josh.w.lewis@gmail.com>
2014-05-18 14:37:56 -0500

    Fix failing tests for unordered hash keys.

--- test/liner/inspectable_test.rb
+++ test/liner/inspectable_test.rb
@@ -1,15 +1,13 @@
 describe Liner::Inspectable do
   subject { Cheeseburger.new(:meat => 'turkey') }
   it "#inspect must include all attributes" do
-    subject.to_s.must_include '#<Cheeseburger'
-    subject.to_s.must_include '>'
+    subject.to_s.must_match(/#\<Cheeseburger\ .*\>/)
     subject.to_s.must_include 'bun=nil'
     subject.to_s.must_include 'meat="turkey"'
     subject.to_s.must_include 'cheese=nil'
   end
   it "#to_s must include all attributes" do
-    subject.to_s.must_include '#<Cheeseburger'
-    subject.to_s.must_include '>'
+    subject.to_s.must_match(/#\<Cheeseburger\ .*\>/)
     subject.to_s.must_include 'bun=nil'
     subject.to_s.must_include 'meat="turkey"'
     subject.to_s.must_include 'cheese=nil'
--- test/liner/serializable_test.rb
+++ test/liner/serializable_test.rb
@@ -3,6 +3,9 @@ describe Liner::Serializable do
   subject { Pizza.new 'thin', 'tomato' }
   describe :to_json do
     it "should render a json string" do
+      subject.to_json.must_match /\{.*\}/
+      subject.to_json.must_include '"crust":"thin"'
+      subject.to_json.must_include '"sauce":"tomato"'
       subject.to_json.must_equal "{\"crust\":\"thin\",\"sauce\":\"tomato\"}"
     end
   end
